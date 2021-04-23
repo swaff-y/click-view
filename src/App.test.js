a5c64bbe52d26e248f5a687b8a05183238572ec7
swaff-y <kyle@swaff.id.au>
2021-04-23 18:59:58 +1000

    Initial Commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import React from 'react'
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// set up enzyme's react adapter
+Enzyme.configure({ adapter: new Adapter});
+
+const setup = (props={}) => {
+    return shallow(<App {...props} />)
+};
+
+const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`);
+
+test("renders without error", ()=>{
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper,"component-app");
+  expect(component.length).toBe(1);
 });
